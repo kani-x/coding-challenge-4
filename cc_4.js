e9fae6d15d304276521b07f73ec4688a0ae1a842
kani <kanishkas@usf.edu>
2025-02-02 20:05:20 -0500

    Task 2 Sales Report (For Loop)

--- cc_4.js
+++ cc_4.js
@@ -1,4 +1,4 @@
-// Task #1
+// Task 1: Customer Discounts (If Statements)
 let purchaseAmount = 120;
 let discount = 0.1; // 10% discount
 
@@ -8,3 +8,12 @@ if (purchaseAmount > 100) {
 
 console.log(`Final amount after discount: $${purchaseAmount}`);
 
+// Task 2 Sales Report (For Loop)
+let sales = [120, 85, 200, 150, 90];
+let totalSales = 0;
+
+for (let i = 0; i < sales.length; i++) {
+    totalSales += sales[i];
+}
+
+console.log(`Total sales: $${totalSales}`);
